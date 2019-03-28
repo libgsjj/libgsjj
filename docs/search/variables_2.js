4d23e6c470dd399418963efae9368a2aa6c8aa8f
Gaëtan Staquet <gaetan.staquet@gmail.com>
2019-03-28 18:44:07 +0100

    Documentation update

--- docs/search/variables_2.js
+++ docs/search/variables_2.js
@@ -4,7 +4,9 @@ var searchData=
   ['m_5fbinarysize',['m_binarySize',['../classgsjj_1_1passive_1_1CNF_1_1BinaryCNFMethod.html#ab6786df8bd2e8ea622852e5b7173e828',1,'gsjj::passive::CNF::BinaryCNFMethod']]],
   ['m_5fcputimeend',['m_cpuTimeEnd',['../classgsjj_1_1passive_1_1Method.html#abcfd5b7be2ff29432d08328810b45408',1,'gsjj::passive::Method']]],
   ['m_5fcputimestart',['m_cpuTimeStart',['../classgsjj_1_1passive_1_1Method.html#ab5066b6102d457181c65fa512b90b4dc',1,'gsjj::passive::Method']]],
+  ['m_5fdvars',['m_dVars',['../classgsjj_1_1passive_1_1CNF_1_1HeuleVerwerCNFMethod.html#afbd4948e522fb1776fa3a8296860cfbf',1,'gsjj::passive::CNF::HeuleVerwerCNFMethod']]],
   ['m_5fexprmanager',['m_exprManager',['../classgsjj_1_1passive_1_1SMT_1_1SMTMethod.html#a7dbd4e7c380baa1c565a6b37d04183c0',1,'gsjj::passive::SMT::SMTMethod']]],
+  ['m_5ffvars',['m_fVars',['../classgsjj_1_1passive_1_1CNF_1_1HeuleVerwerCNFMethod.html#a22a6af7fcd1d9141c3f11eaa784d067c',1,'gsjj::passive::CNF::HeuleVerwerCNFMethod']]],
   ['m_5fhassolution',['m_hasSolution',['../classgsjj_1_1passive_1_1Method.html#acd6b5238d4cbfbc6db948017d8fd0999',1,'gsjj::passive::Method']]],
   ['m_5fnumberstates',['m_numberStates',['../classgsjj_1_1passive_1_1Method.html#ad5180d9fcd2df8c86033b97618bbc32e',1,'gsjj::passive::Method']]],
   ['m_5fprefixes',['m_prefixes',['../classgsjj_1_1passive_1_1Method.html#a2ba85f0bbd5399a61114a5c4ad0f6ab7',1,'gsjj::passive::Method']]],
@@ -15,5 +17,6 @@ var searchData=
   ['m_5fsolver',['m_solver',['../classgsjj_1_1passive_1_1CNF_1_1BinaryCNFMethod.html#a4bea21993718c5a5487ccf7d158e5c33',1,'gsjj::passive::CNF::BinaryCNFMethod']]],
   ['m_5fsp',['m_Sp',['../classgsjj_1_1passive_1_1Method.html#a0217ec5813e210a39bd7afcda00714a8',1,'gsjj::passive::Method']]],
   ['m_5fstatetovar',['m_stateToVar',['../classgsjj_1_1passive_1_1CNF_1_1BinaryCNFMethod.html#aecb613ad2f2d2ac5a139fc0f5e0d09ae',1,'gsjj::passive::CNF::BinaryCNFMethod']]],
-  ['m_5ftriedsolve',['m_triedSolve',['../classgsjj_1_1passive_1_1Method.html#a587f941ba89bcbfe26d14bc58c9433ba',1,'gsjj::passive::Method']]]
+  ['m_5ftriedsolve',['m_triedSolve',['../classgsjj_1_1passive_1_1Method.html#a587f941ba89bcbfe26d14bc58c9433ba',1,'gsjj::passive::Method']]],
+  ['m_5fxvars',['m_xVars',['../classgsjj_1_1passive_1_1CNF_1_1HeuleVerwerCNFMethod.html#affda17716e9848dc029dcf1669c88ba5',1,'gsjj::passive::CNF::HeuleVerwerCNFMethod']]]
 ];
